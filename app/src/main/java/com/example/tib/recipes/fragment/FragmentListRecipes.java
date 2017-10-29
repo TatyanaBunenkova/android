1378fea3c8e46493623d5756636f0358c0fc6d26
Tatyana <tbunenkova@yandex.ru>
2017-10-29 16:02:58 +0300

    First Commit

---
+++ app/src/main/java/com/example/tib/recipes/fragment/FragmentListRecipes.java
@@ -0,0 +1,62 @@
+package com.example.tib.recipes.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tib.recipes.R;
+
+
+public class FragmentListRecipes extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public FragmentListRecipes() {}
+
+    public static FragmentListRecipes newInstance() {
+        FragmentListRecipes fragment = new FragmentListRecipes();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_recipes, container, false);
+
+        //ТУТ
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
